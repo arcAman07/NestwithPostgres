9bad6ad77e9e3c8ad9e914883c6b587f0456f08b
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-06 01:42:14 +0530

    Initial boilerplate

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
+import { TaskRepository } from './task.repository';
 import { TaskService } from './task.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TaskRepository])],
   controllers: [TaskController],
   providers: [TaskService],
 })
