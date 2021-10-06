2ffcfc3e67fd8d060a841a9d6e28f5d9b740c297
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-07 02:06:07 +0530

    user repository refactored

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -4,11 +4,10 @@ import { User } from './user.entity';
 import { registerUser } from './dto/create-user.dto';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    async createUser(registerUserinfo:registerUser):Promise<User>{
+    async createUser(registerUserinfo:registerUser):Promise<void>{
         const {username, password} = registerUserinfo;
         const user = this.create({username, password});
         await this.save(user);
-        return user;
     }
 
 }
