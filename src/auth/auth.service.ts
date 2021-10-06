0e287b11ac581331c2fd11337d2ae3f6a6d09b15
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-07 02:19:17 +0530

    Register User route added

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { registerUser } from './dto/create-user.dto';
+import { UserRepository } from './user.repository';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectRepository(UserRepository) private userRepository: UserRepository,
+  ) {}
+  async signUp(registerUserinfo: registerUser): Promise<void> {
+    await this.userRepository.createUser(registerUserinfo);
+  }
+}
