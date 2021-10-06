0e1b42fe05579f0ebbb5a35849cf8b990c8687a4
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-07 02:30:31 +0530

    class validator added

--- src/auth/dto/create-user.dto.ts
+++ src/auth/dto/create-user.dto.ts
@@ -1,9 +1,15 @@
 /* eslint-disable prettier/prettier */
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class registerUser {
     @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     username: string;
     @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(20)
     password: string;
 }
