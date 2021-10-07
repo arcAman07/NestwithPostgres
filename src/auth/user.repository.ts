ed1c1374174e9b046ffd8a318fd01f2234cadc8a
Aman <aman.sharma2020b@vitstudent.ac.in>
2021-10-07 16:57:32 +0530

    password encryption added

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -2,12 +2,27 @@
 import { EntityRepository, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { registerUser } from './dto/create-user.dto';
+import { ConflictException, InternalServerErrorException } from '@nestjs/common';
+import * as bcrypt from 'bcryptjs';
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
     async createUser(registerUserinfo:registerUser):Promise<void>{
         const {username, password} = registerUserinfo;
-        const user = this.create({username, password});
-        await this.save(user);
+        const salt  = await bcrypt.genSaltSync(10);
+        const hashedPassword = bcrypt.hashSync(password,salt); 
+        const user = this.create({username, password:hashedPassword});
+        try {
+            await this.save(user);
+        } catch(error) {
+            console.log(error.code, error.message);
+            if (error.code === "23505") {
+                throw new ConflictException("username already exists");
+            }
+            else {
+                throw new InternalServerErrorException();
+            }
+        }
+        
     }
 
 }
